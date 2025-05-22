57ece91fa73136d6d4a51000f8bab4e57cba66dc
HuongDS <146117043+HuongDS@users.noreply.github.com>
2025-05-22 15:21:44 +0700

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,16 +1,15 @@
 <?php
-    $hostName = "127.0.0.1"; 
-    $port = "3306";          
-    $dbName = "misc";
-    $userName = "fred";
-    $password = "alan";
+$host = '127.0.0.1';
+$port = '3306';
+$db   = 'misc';
+$user = 'fred';
+$pass = 'alan';
 
-    $pdo = null;
-
-    try {
-        $pdo = new PDO("mysql:host=$hostName;port=$port;dbname=$dbName", $userName, $password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch(PDOException $e) {
-        echo "<p style='color:red'>Connection failed: " . htmlentities($e->getMessage()) . "</p>";
-    }
+$pdo = null;
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "<p style='color:red'>Connection failed: " . htmlentities($e->getMessage()) . "</p>";
+}
 ?>
